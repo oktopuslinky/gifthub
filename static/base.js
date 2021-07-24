3fff10ffa20c40bdb8df553d206fbf05216e204c
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2021-07-25 01:17:55 +0530

    navbar

---
+++ static/base.js
@@ -0,0 +1,9 @@
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+    menuBtnChange();
+});
+
+
