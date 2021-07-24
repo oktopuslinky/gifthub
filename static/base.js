35b10ed14d0f22987ae79b50927f41ad6acbed2f
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2021-07-25 03:55:42 +0530

    Added a SPLENDID navbar

---
+++ static/base.js
@@ -0,0 +1,7 @@
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+
+closeBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("open");
+    menuBtnChange();
+});
